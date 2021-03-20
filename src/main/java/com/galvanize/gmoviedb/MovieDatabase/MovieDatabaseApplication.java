7016a04d3a6169c99801a946978a11d4b63f0d76
404-bug-notfound <iqbal.jamadar@gmail.com>
2021-03-20 10:58:04 -0400

    Initial project setup

---
+++ src/main/java/com/galvanize/gmoviedb/MovieDatabase/MovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.gmoviedb.MovieDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDatabaseApplication.class, args);
+	}
+
+}
